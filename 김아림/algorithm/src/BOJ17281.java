81d566c8a1773cb7f38c2ae9f709d230999e69f1
김아림 <kal72222222@gmail.com>
2023-08-23 00:30:44 +0900

    BOJ17281

--- 김아림/algorithm/src/BOJ17281.java
+++ 김아림/algorithm/src/BOJ17281.java
@@ -13,7 +13,7 @@ public class BOJ17281 {
     static int N;
     static boolean[] select;
     static int[] playerQ;
-    static int[] base;
+    static boolean[] base;
     static int ans = Integer.MIN_VALUE;
     static int score;
 
@@ -27,18 +27,19 @@ public class BOJ17281 {
         select = new boolean[9];
         playerQ = new int[9];
 
-        base = new int[4]; // 첫번째 쳐서 출루한사람, 두번째 쳐서 출루한사람, 세번째 쳐서 출루한 사람
+        base = new boolean[4]; // 타자, 1루 진출, 2루 진출, 3루 진출
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 9; j++) {
-                player[i][i] = Integer.parseInt(st.nextToken());
+                player[j][i] = Integer.parseInt(st.nextToken());
             }
         }
 
         // 4번 타자는 정해져 있으므로 4번 빼고 정해야함요
 
         makeQ(0);
+        System.out.println(ans);
 
     }
 
@@ -54,8 +55,7 @@ public class BOJ17281 {
         if (src == 9) {
             score = 0;
             playerQ[3] = 0;
-            System.out.println(Arrays.toString(playerQ));
-            // 여기서 계산
+            base = new boolean[4];
             calc();
             ans = Math.max(ans, score);
             return;
@@ -77,29 +77,38 @@ public class BOJ17281 {
 
         // 이때 모든 출루는 다 취소되므로 점수는 이닝마다 0으로 초기화 해서 계산함
         int now_player = 0; // 0번 플레이어 부터 시작함 -> playerQ의 인덱스가됨
+
         for (int i = 0; i < N; i++) { // 이닝 만큼 반복함
             int outCount = 0; // outCount가 9가 되면 다음 이닝을 시작함
-            int base = -3; // base에서 부터 3명이 출루해야지 올라감
-
+            Arrays.fill(base, false); // 출루 X
             while (outCount < 3) {
-                int now_score = player[now_player][i];
+                base[0] = true; // 타자 준비
+                int now_score = player[playerQ[now_player]][i]; // 한 선수의 i번째 이닝때 점수
 
                 switch (now_score) {
                     case 0:
                         outCount++; // 아웃카운트 증가
                         break;
                     case 1: // 안타
+                        makeScore(1);
                         break;
                     case 2: // 2루타
+                        makeScore(2);
                         break;
                     case 3: // 3루
+                        makeScore(3);
                         break;
                     case 4:
+                        for (int j = 0; j < 4; j++) {
+                            if (base[j]) score++; // 각 베이스에 있던 선수들 모두 점수 얻음 + 타자
+                        }
+                        Arrays.fill(base, false); // 출루 X
+                        base[0] = true; //
                         break;
                 }
 
                 now_player++; // 어떤 경우에도 한번 치면 선수 교체
-                if(now_player == 9){
+                if (now_player == 9) {
                     now_player = 0;
                 }
 
@@ -110,16 +119,29 @@ public class BOJ17281 {
     }
 
 
-    static void makeScore(int i) {
+    static void makeScore(int s) {
 
-        // 만약에 3루에 사람이 있으면 -> 점수 내고 한칸씩 땡김
-        // 뒤에서 부터 땡겨야함. 3루 먼저 검사하는게 필수일 듯
-        // 점수 내고 0 만들고 그다음에 뒤에서 부터 돌면서 댕기기
-        // 사람이 들어오는 경우엔 사람을 넣어주고
-        // 홈런의 경우 사람이 들어오지 않음! 그냥 전체 배열을 돌면서 확인
+        for (int j = 0; j < s; j++) {
+            // 0 은 타자 자리
+            if (base[3]) {
+                score++; // 점수 생성 및
+            }
 
-        // 만약에 사람이 없으면 (그냥 땡김)
-        // 그걸 점수 만큼 반복
+            // 한칸씩 땡기기
+
+            base[3] = base[2]; // 2루의 선수가 3루로
+            base[2] = base[1]; // 1루의 선수가 2루로
+            base[1] = base[0];
+            // 만약에 3루에 사람이 있으면 -> 점수 내고 한칸씩 땡김
+            // 뒤에서 부터 땡겨야함. 3루 먼저 검사하는게 필수일 듯
+            // 점수 내고 0 만들고 그다음에 뒤에서 부터 돌면서 댕기기
+            // 사람이 들어오는 경우엔 사람을 넣어주고
+            // 홈런의 경우 사람이 들어오지 않음! 그냥 전체 배열을 돌면서 확인
+
+            // 만약에 사람이 없으면 (그냥 땡김)
+            // 그걸 점수 만큼 반복
+        }
+
     }
 
 }
